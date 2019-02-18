2a3a30d13219a7e4eab32e8a63ac28f2858384ea
pranotobudi <pranotobudi.s@gmail.com>
2019-02-18 16:43:43 +0700

    change setting for postgres database production on heroku

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -13,10 +13,10 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd12cjbiskmp7sr',
-        'USER': 'nrpfrigvkupqhy',
-        'PASSWORD': 'bcb5e85dd342127c30c0880e8b6c6184a051a7368a1639ec27e8a1279c762472',
-        'HOST': 'ec2-54-83-50-174.compute-1.amazonaws.com',
+        'NAME': 'dbltu8bfhoh35v',
+        'USER': 'iijrpjzngvwxra',
+        'PASSWORD': 'e89d41465947093ddf6dfddb2010389997e01d87851780e17bd5e4e12ec5779b',
+        'HOST': 'ec2-174-129-224-157.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }

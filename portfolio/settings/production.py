3695b5ae2468d6d977bf27ef4b6070cc935ddff1
pranotobudi <pranotobudi.s@gmail.com>
2019-02-18 16:50:33 +0700

    edit ALLOWED_HOST

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = False
-ALLOWED_HOST = ['pranotobudi-website.herokuapp.com/']
+ALLOWED_HOST = ['pranotobudi-website.herokuapp.com']
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
